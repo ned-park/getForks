0e49b2f185dd5de43dd0118c214a36b00961677d
ned-park <87483870+ned-park@users.noreply.github.com>
2023-02-03 15:55:39 -0500

    feat: add Recipe model

---
+++ models/Recipe.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const RecipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+    index: true
+  },
+  instructions: {
+    type: [String],
+    required: true
+  }, 
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  clonedFrom: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false
+  },
+  repo: {
+    type: mongoose.Schema.Types.ObjectId,
+    red: 'Repo',
+    required: false
+  }
+})
+
+export default mongoose.model('Recipe', RecipeSchema)
