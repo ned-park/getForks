47fd0f2c5c387e68c8e0544a2e25e3efa4753ed7
ned-park <87483870+ned-park@users.noreply.github.com>
2023-02-03 16:01:13 -0500

    feat: add User model

---
+++ models/User.js
@@ -0,0 +1,61 @@
+import bcrypt from "bcrypt"
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  email: { type: String, unique: true },
+  password: String,
+  repos: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Repo'
+      }
+    ],
+    required: true,
+    default: []
+  }
+  }, 
+  { toJSON: { 
+    transform: function(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.password;
+    }
+  }, 
+})
+
+// Password hash middleware.
+ 
+ UserSchema.pre('save', function save(next) {
+  const user = this
+  if (!user.isModified('password')) { return next() }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) { return next(err) }
+      user.password = hash
+      next()
+    })
+  })
+})
+
+// Helper method for validating user's password.
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch)
+  })
+}
+
+UserSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+
+export default mongoose.model('User', UserSchema)
