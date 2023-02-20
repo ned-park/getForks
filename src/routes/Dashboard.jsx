0eb05290eb04919396a04bbe8caeb3362fa3339f
ned-park <87483870+ned-park@users.noreply.github.com>
2023-02-19 19:52:51 -0500

    feat: fetch repositories from API for user dashboard

---
+++ src/routes/Dashboard.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import { useAuthContext } from "../hooks/useAuthContext"
+import RepoCard from "../components/RepoCard"
+
+export default function Dashboard() {
+  let [repos, setRepos] = useState([])
+  let [loaded, setLoaded] = useState(false)
+
+  const { user, username } = useAuthContext()
+  
+  useEffect(() => {
+    const fetchRepo = () => {
+      fetch(`/api/${username}`, {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        setRepos([...data.repos])
+      })
+    }
+
+    if (user && user.user) {
+      fetchRepo()
+    }
+  }, [user])
+
+
+  return (
+    <main>
+      <h1>Recipes</h1>
+      {repos.map(repo => <RepoCard repo={repo} key={repo.key} />)
+      }
+    </main>
+  )
+}
