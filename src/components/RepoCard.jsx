92d826247bbb5ff17f74d334d8dca47d8f4a0dda
ned-park <87483870+ned-park@users.noreply.github.com>
2023-02-22 19:34:39 -0500

    feat: switch links to NavLinks

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { NavLink } from "react-router-dom"
 
 export default function RepoCard({repo}) {
   return (
@@ -7,12 +7,12 @@ export default function RepoCard({repo}) {
           <div className="">             
                 {repo.image && <img className="" src={`${repo.image.slice(0,49)}/f_auto,q_90,w_256/${repo.image.slice(62)}`} alt="" />}
               <div className="">
-                  <a href={`${repo.username}/${repo._id}`}>
+                  <NavLink to={`/${repo.username}/${repo._id}`}>
                       <h5 className="">{repo.title}</h5>
-                  </a>
-                  <a href={`${repo.username}`} className="">
+                  </NavLink>
+                  <NavLink to={`/${repo.username}`} className="">
                     <span className="">More from {repo.username}</span>
-                    </a>
+                    </NavLink>
                   <div className="">
                    {repo.description.length > 0 && 
                       <span className="">{repo.description.slice(0,128) + '...'}</span>
@@ -20,7 +20,7 @@ export default function RepoCard({repo}) {
                   </div>
                   {repo.tags && (
                     <div className="">
-                      {repo.tags.map((tag, i) => (<a href={`/search?query=${tag}`} key={`tag${i}`} className="hover:text-blue cursor:pointer"><span key={`tagspan${i}`}>{tag} </span></a>) )}
+                      {repo.tags.map((tag, i) => (<NavLink to={`/search?query=${tag}`} key={`tag${i}`} className="hover:text-blue cursor:pointer"><span key={`tagspan${i}`}>{tag} </span></NavLink>) )}
                     </div>
                   )}
                   {/* <!-- <p className="text-brBlack text-xs">=repo.creationDate</p> --> */}
