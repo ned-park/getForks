9af2e5a5121dec770bfa403c4ba63daaff105cb2
ned-park <87483870+ned-park@users.noreply.github.com>
2023-03-09 00:11:40 -0500

    feat: style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./Navbar";
 
-export default function Header() {
+export default function Header({ setTheme }) {
   return (
-    <header className="header">
-      <div className="container flex">
-        <span>getForks</span>
-        <Navbar />
+    <header className="bg-primary p-6">
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl text-primary">getForks</h2>
+        <Navbar setTheme={setTheme} />
       </div>
     </header>
   );
