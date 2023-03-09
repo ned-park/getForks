db0ce5ff340899d1f05e4e8189e61dc3ebb02321
ned-park <87483870+ned-park@users.noreply.github.com>
2023-03-09 00:29:41 -0500

    feat: add persistent theme switcher

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+const generateColorClass = (variable) => {
+  return ({ opacityValue }) =>
+    opacityValue
+      ? `rgba(var(--${variable}), ${opacityValue})`
+      : `rgb(var(--${variable}))`;
+};
+
+const textColor = {
+  primary: generateColorClass("text-primary"),
+  secondary: generateColorClass("text-secondary"),
+  tertiary: generateColorClass("text-tertiary"),
+  accent: generateColorClass("text-accent"),
+};
+
+const backgroundColor = {
+  primary: generateColorClass("bg-primary"),
+  secondary: generateColorClass("bg-secondary"),
+  tertiary: generateColorClass("bg-tertiary"),
+};
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor,
+      backgroundColor,
+    },
   },
+
   plugins: [],
 };
