214a8205470e40e02f6e03671e871d95c4bbeab3
ned-park <87483870+ned-park@users.noreply.github.com>
2023-03-15 14:44:48 -0400

    feat: add desktop styling

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -2,57 +2,57 @@ import { NavLink } from "react-router-dom";
 
 export default function RepoCard({ repo }) {
   return (
-    <section className="repoCard">
-      <div className="">
-        <div className="">
+    <section className="bg-primary text-primary repoCard flex justify-start shadow-lg lg:min-h-[192px] lg:max-h-[192px] rounded">
+      <NavLink to={`/${repo.userId.username || repo.username}/${repo._id}`}>
+        <div className="min-w-[256px]">
           {repo.image && (
             <img
-              className=""
+              className="w-256 lg:rounded-l"
               src={`${repo.image.slice(
                 0,
                 49
-              )}/f_auto,q_90,w_256/${repo.image.slice(62)}`}
+              )}/f_auto,q_90,c_fill,h_192,w_256/${repo.image.slice(62)}`}
               alt=""
             />
           )}
+        </div>
+      </NavLink>
+      <div className="flex flex-col w-full px-4 p-2">
+        <NavLink to={`/${repo.userId.username || repo.username}/${repo._id}`}>
           <div className="">
-            <NavLink
-              to={`/${repo.userId.username || repo.username}/${repo._id}`}
-            >
-              <h5 className="">{repo.title}</h5>
-            </NavLink>
-            <NavLink
-              to={`/${repo.userId.username || repo.username}`}
-              className=""
-            >
-              <span className="">
-                More from {repo.userId.username || repo.username}
-              </span>
-            </NavLink>
-            <div className="">
+            <h5 className="text-center font-bold mt-2">{repo.title}</h5>
+            <div className="mt-4">
               {repo.description.length > 0 && (
                 <span className="">
                   {repo.description.slice(0, 128) + "..."}
                 </span>
               )}
             </div>
-            {repo.tags && (
-              <div className="">
-                {repo.tags.map((tag, i) => (
-                  <NavLink
-                    to={`/search?query=${tag}`}
-                    key={`tag${i}`}
-                    className="hover:text-blue cursor:pointer"
-                  >
-                    <span key={`tagspan${i}`}>{tag} </span>
-                  </NavLink>
-                ))}
-              </div>
-            )}
-            {/* <!-- <p className="text-brBlack text-xs">=repo.creationDate</p> --> */}
           </div>
+        </NavLink>
+        <div className="flex mt-auto items-end justify-between text-sm">
+          {repo.tags && (
+            <div>
+              {repo.tags.length > 0 && <span> More tagged with: </span>}
+              {repo.tags.map((tag, i) => (
+                <NavLink
+                  to={`/search?query=${tag}`}
+                  key={`tag${i}`}
+                  className="text-primary hover:text-accent cursor:pointer"
+                >
+                  <span key={`tagspan${i}`}>{tag} </span>
+                </NavLink>
+              ))}
+            </div>
+          )}
+          <NavLink to={`/${repo.userId.username || repo.username}`}>
+            <span className="">
+              More from {repo.userId.username || repo.username}
+            </span>
+          </NavLink>
         </div>
       </div>
+      {/* <!-- <p className="text-brBlack text-xs">=repo.creationDate</p> --> */}
     </section>
   );
 }
