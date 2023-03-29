e04b454e8eea025b428170fd237b10ba0edba797
ned-park <87483870+ned-park@users.noreply.github.com>
2023-03-28 22:32:47 -0400

    build: fix deployment

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.use("/api", mainRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/:user", dashboardRoutes);
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
