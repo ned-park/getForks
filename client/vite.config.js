7e460461822147cdfa04ffda68ebad7ba6f74731
ned-park <87483870+ned-park@users.noreply.github.com>
2023-03-28 21:51:22 -0400

    build: revamp start script

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-      },
+      "/api": "http://localhost:3000",
     },
   },
   plugins: [react()],
