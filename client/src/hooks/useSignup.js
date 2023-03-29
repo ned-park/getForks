196c7971291fd806ba3558dff286624465920ea5
ned-park <87483870+ned-park@users.noreply.github.com>
2023-03-28 23:10:49 -0400

    feat: signup redirection to home page

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useSignup = () => {
+  const navigate = useNavigate();
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
 
@@ -26,6 +28,7 @@ export const useSignup = () => {
       localStorage.setItem("user", JSON.stringify(json));
       dispatch({ type: "LOGIN", payload: json });
       setIsLoading(false);
+      navigate(`/`);
     }
   };
   return { signup, isLoading, error };
