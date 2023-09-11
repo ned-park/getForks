a394199d304b28eda91ba9c1f902303dcc676866
ned-park <87483870+ned-park@users.noreply.github.com>
2023-09-11 14:03:05 -0400

    feat: add demo user login info

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -26,6 +26,11 @@ export default function Login() {
   return (
     <section className="mt-24 mx-auto container flex flex-col px-2">
       <h2 className="text-xl font-bold text-center mb-6">Login</h2>
+      <div className="mx-auto text-center mb-3">
+        <h4 className="font-bold">For demo</h4>
+        <p>Username: demo</p>
+        <p>Password: demo</p>
+        </div>
       <Form
         handleClick={handleSubmit}
         handleChange={handleChange}
